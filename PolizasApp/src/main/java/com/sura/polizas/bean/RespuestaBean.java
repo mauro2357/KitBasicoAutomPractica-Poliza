055753a67a62c4045f52aa94cb8860f1af6db575
David <dgomez1285@gmail.com>
2018-05-15 16:25:23 -0500

    controlladors

---
+++ PolizasApp/src/main/java/com/sura/polizas/bean/RespuestaBean.java
@@ -0,0 +1,22 @@
+package com.sura.polizas.bean;
+
+public class RespuestaBean {
+
+	private boolean financiado;
+	private Long valorPoliza;
+	public boolean isFinanciado() {
+		return financiado;
+	}
+	public void setFinanciado(boolean financiado) {
+		this.financiado = financiado;
+	}
+	public Long getValorPoliza() {
+		return valorPoliza;
+	}
+	public void setValorPoliza(Long valorPoliza) {
+		this.valorPoliza = valorPoliza;
+	}
+	
+	
+	
+}
--- PolizasApp/src/main/java/com/sura/polizas/controller/PolizasController.java
+++ PolizasApp/src/main/java/com/sura/polizas/controller/PolizasController.java
@@ -1,15 +1,25 @@
 package com.sura.polizas.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.sura.polizas.bean.RespuestaBean;
 
 @RestController
 @RequestMapping("/api/polizas")
 public class PolizasController {
 
-	 @GetMapping("/validar")
-	 public String validar() {
-	     return "OK";
+	
+	
+	
+	 @GetMapping("/{idPoliza}")
+	 public RespuestaBean getPoliza(@PathVariable(value = "idPoliza") Long id) {
+		 
+		 RespuestaBean respuestaBean = new RespuestaBean();
+		 respuestaBean.setFinanciado(true);
+		 respuestaBean.setValorPoliza(1000L);
+	     return respuestaBean;
 	 }
 }
