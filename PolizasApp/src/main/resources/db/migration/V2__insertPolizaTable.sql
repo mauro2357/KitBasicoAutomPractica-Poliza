ed4768d154f4c6974b8b4e0610abafdf51a493d3
Lenovo-PC\JUAN CARLOS <juan.castro.mere@gmail.com>
2018-05-15 17:58:06 -0500

    base de datos

---
+++ PolizasApp/src/main/resources/db/migration/V2__insertPolizaTable.sql
@@ -0,0 +1,4 @@
+insert into poliza (TIPO_POLIZA, EMPRESA, VALOR_POLIZA) values(1,"SURA", 100000);
+insert into poliza (TIPO_POLIZA, EMPRESA, VALOR_POLIZA) values(1,"CMD", 50000);
+insert into poliza (TIPO_POLIZA, EMPRESA, VALOR_POLIZA) values(1,"MML", 20000);
+insert into poliza (TIPO_POLIZA, EMPRESA, VALOR_POLIZA) values(2,"", 100000);
