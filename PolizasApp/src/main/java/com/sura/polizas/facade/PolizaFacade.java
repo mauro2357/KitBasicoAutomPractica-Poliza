384e16650afb85585ed1b2de0f50885e31426831
David <dgomez1285@gmail.com>
2018-05-16 15:58:19 -0500

    fix

--- PolizasApp/src/main/java/com/sura/polizas/facade/PolizaFacade.java
+++ PolizasApp/src/main/java/com/sura/polizas/facade/PolizaFacade.java
@@ -12,38 +12,43 @@ import com.sura.polizas.entidades.Poliza;
 import com.sura.polizas.entidades.TipoPoliza;
 
 @Component
-public class PolizaFacade implements IPolizaFacade{
+public class PolizaFacade implements IPolizaFacade {
 
 	@Autowired
 	PolizasRepository polizasRepository;
-	
+
 	public Poliza findById(long id) {
-		return polizasRepository.findById(id)
-        .orElse(new Poliza());
+		return polizasRepository.findById(id).orElse(new Poliza());
 	}
-	
+
 	public RespuestaValidaBean validaPedido(long id, String tipoDocumento, String numeroDocumento, String placa) {
 		Poliza poliza = findById(id);
-		if(poliza==null) return RespuestaValidaBean.buildNoFinanciado("No existe la poliza con el id "+id) ;
+		if (poliza == null)
+			return RespuestaValidaBean.buildNoFinanciado("No existe la poliza con el id " + id);
 
 		RestTemplate restTemplate = new RestTemplate();
-		if(poliza.getTipoPoliza()==TipoPoliza.COLECTIVO.codigo) {
+		if (poliza.getTipoPoliza() == TipoPoliza.COLECTIVO.codigo) {
 			Asegurado asegurado = new Asegurado(tipoDocumento, numeroDocumento, new Long(id).toString());
-			ResponseEntity<RespuestaValidaBean> rpta = restTemplate.postForEntity("http://172.16.0.123:6666/api/asegurado/valida", asegurado, RespuestaValidaBean.class);
+			ResponseEntity<RespuestaValidaBean> rpta = restTemplate.postForEntity(
+					"http://172.16.0.123:6666/api/asegurado/valida", asegurado, RespuestaValidaBean.class);
 			RespuestaValidaBean respuesta = rpta.getBody();
-			if(!respuesta.isAsegurable()) return RespuestaValidaBean.buildNoFinanciado("El duenio del vehiculo no es empleado de la empresa") ;
-			
-		}else if(poliza.getTipoPoliza()==TipoPoliza.INDIVIDUAL.codigo) {
-			ResponseEntity<String> rpta = restTemplate.getForEntity("http://172.16.0.123:8585/api/vehiculo/zona/"+placa, String.class);
-			String respuesta = rpta.getBody();
-			if(respuesta==null || !respuesta.equals(poliza.getZona()) ) return RespuestaValidaBean.buildNoFinanciado("La zona del vehiculo no aplica para la poliza") ;
-			
-		}else {
-			return RespuestaValidaBean.buildNoFinanciado("La poliza es de un tipo no conocido ") ;
-			
+			if (!respuesta.isAsegurable())
+				return RespuestaValidaBean.buildNoFinanciado("El duenio del vehiculo no es empleado de la empresa");
+
+		} else if (poliza.getTipoPoliza() == TipoPoliza.INDIVIDUAL.codigo) {
+			ResponseEntity<String> rpta = restTemplate
+					.getForEntity("http://172.16.0.123:8585/api/vehiculo/zona/" + placa, String.class);
+			String respuesta = "";
+			respuesta = rpta.getBody();
+			if (respuesta.equals("") || !respuesta.equals(poliza.getZona()))
+				return RespuestaValidaBean.buildNoFinanciado("La zona del vehiculo no aplica para la poliza");
+
+		} else {
+			return RespuestaValidaBean.buildNoFinanciado("La poliza es de un tipo no conocido ");
+
 		}
-		
-		return RespuestaValidaBean.buildFinanciado() ;
+
+		return RespuestaValidaBean.buildFinanciado();
 	}
-	
+
 }
